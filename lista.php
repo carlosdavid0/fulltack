70ea47e666c7f58086f9efc2c1928ac2b44bc3cb
Carlos David <romao.carlosdavi@gmail.com>
2021-02-05 20:34:12 -0300

    alpha

---
+++ lista.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
+    <link rel="stylesheet" href="./styles/list.css">
+</head>
+<style>
+</style>
+
+<body>
+
+
+    <div class="container">
+        <div class="header container">
+            <img src="./assets/logo.svg" class="p-2 img-fluid " alt="">
+
+            <div class="options container d-flex align-items-center justify-content-between p-2 mt-3">
+
+            <div class="back">
+             <a href="./" class="text-success"><i class="fas fa-arrow-left " style="font-size: 9mm;"></i></a>
+             </div>
+                <div class="title">
+                <h1 class="h1 container ">Listagem</h1></div>
+            </div>
+
+            </div>
+            
+            
+        </div>
+        <div class="map">
+            
+        <div class="card border-0 m-4 user">
+                <img src=" https://avatars.githubusercontent.com/u/58746461?s=460&u=a5f1ba8aa962a3a650ac4bc0694d50b35df21656&v=4" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Carlos David</h5>
+                    <p class="card-text">CTO On Central das Balas e programador web Full-Stack </p>
+                    <div class="action d-flex justify-content-center">
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-github"></i></a>
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="card border-0 m-4 user">
+                <img src=" https://avatars.githubusercontent.com/u/58746461?s=460&u=a5f1ba8aa962a3a650ac4bc0694d50b35df21656&v=4" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Carlos David</h5>
+                    <p class="card-text">CTO On Central das Balas e programador web Full-Stack </p>
+                    <div class="action d-flex justify-content-center">
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-github"></i></a>
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="card border-0 m-4 user">
+                <img src=" https://avatars.githubusercontent.com/u/58746461?s=460&u=a5f1ba8aa962a3a650ac4bc0694d50b35df21656&v=4" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Carlos David</h5>
+                    <p class="card-text">CTO On Central das Balas e programador web Full-Stack </p>
+                    <div class="action d-flex justify-content-center">
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-github"></i></a>
+                    <a href="#" class="btn btn-primary ml-3"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+</html>
